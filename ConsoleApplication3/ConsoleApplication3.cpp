322fec62b10fb38747f0d1ff066004101a4e9bdc
kaoren1 <yaroslavrusakevich@gmail.com>
2024-01-30 14:20:50 +0300

    a

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+//11 вариант
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b;
+    cout << "Введите значение первой перменной: ";
+    cin >> a;
+    cout << "Введите значение второй перменной: ";
+    cin >> b;
+
+    int* pA = &a;
+    int* pB = &b;
+
+    int c = *pA;
+    *pA = *pB;
+    *pB = c;
+
+    cout << "Значение первой переменной: " << a << endl;
+    cout << "Значение второй переменной: " << b << endl;
+}
